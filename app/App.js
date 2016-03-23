45a5445e35f0b2cb1ee149bff03f0b5ff237e2ff
Bryan Fillmer <bryan.fillmer@effectiveui.com>
2016-03-23 06:11:45 -0600

    Linting and basic npm script setup.
    Extending Airbnb's linting, cleaned up what little code exists to match said linter. Added
    some utility scripts to the package.json.

--- app/App.js
+++ app/App.js
@@ -1,10 +1,12 @@
 
-import React from 'react'
+import React from 'react';
 
-const App = ({}) => {
-  return (
-    <div className = 'container-app'></div>
-  )
-}
+const App = () => (
+  <div className = "container-app">
+    <h1>
+      Basic Boilerplate
+    </h1>
+  </div>
+);
 
-export default App
+export default App;
