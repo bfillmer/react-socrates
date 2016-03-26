fc67de74a1d8b4547407a4813d5778b0d1a5b8ee
Bryan Fillmer <bryan.fillmer@effectiveui.com>
2016-03-26 15:15:38 -0600

    Basic Socrates subscription working.
    App container currently unwraps Socrates store and show updated state
    as expected. Proof-of-concept code that will be redone in next update.

--- app/App/Container.js
+++ app/App/Container.js
@@ -1,14 +1,47 @@
 
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const App = () => {
-  return (
-    <div className = "container-app">
-      <h1>
-        Basic Boilerplate!
-      </h1>
-    </div>
-  );
+/**
+ * @NOTE
+ * Currently app takes the prop store, the Socrates-wrapped store, subscribes to it,
+ * and can pass along the internal state of store to children. Extend this into
+ * a standard reusable component that can be reused by any container. Thus any
+ * stateless components will simple pass along the store, if needed, or be getting
+ * "unwrapped" data from their parent container.
+ */
+
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      store: {},
+    };
+  }
+
+  componentWillMount () {
+    this.setState({
+      store: this.props.store(),
+    });
+    this.props.store.subscribe((s) => this.setState({
+      store: s,
+    }));
+  }
+
+  // Pass this.state.store down as the Redux store.
+  render () {
+    return (
+      <div className = "container-app">
+        <h1>
+          Basic Boilerplate! { this.state.store.test }
+        </h1>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  store: PropTypes.func.isRequired,
 };
 
 export default App;
--- app/index.js
+++ app/index.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import store from './store';
+
 import App from 'App/Container';
 
 render(
-  <App />,
+  <App store = { store } />,
   document.getElementById('app')
 );
